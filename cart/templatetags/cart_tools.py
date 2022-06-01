bb904e621e507de83a1dc6f4f6cfc9aca28cecca
Abhisek Joshi <nofursad@gmail.com>
2022-06-01 05:13:19 +0000

    Create the templates for toast to display the user activities on the website.

--- cart/templatetags/cart_tools.py
+++ cart/templatetags/cart_tools.py
@@ -2,6 +2,8 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
+    """Calculating the subtotal for individual item in the cart """
     return price * quantity
